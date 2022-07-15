f889a4b1a3b139057725b2ee81e986c0d12e6be4
VenD <gulraiz.hashmi@ext.careem.com>
2022-07-15 15:26:39 +0500

    first commit

--- src/main/java/testscripts/firstTest.java
+++ src/main/java/testscripts/firstTest.java
@@ -12,7 +12,7 @@ public class firstTest {
     public void testOne() throws MalformedURLException {
         System.out.println(System.getenv("HUB_HOST"));
         DesiredCapabilities dc = DesiredCapabilities.chrome();
-        URL url= new URL("http://localhos"+System.getenv("HUB_HOST")+":4444/wd/hub");
+        URL url= new URL("http://localhost:4444/wd/hub");
         RemoteWebDriver driver = new RemoteWebDriver(url,dc);
         driver.get("http://www.google.com");
         System.out.println("Title of trehe page: "+driver.getTitle());
